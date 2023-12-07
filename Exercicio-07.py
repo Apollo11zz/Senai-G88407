be4cc43eaff059e56ba6bd386c4aec0410311103
Apollo11zz <victorgabrieldejesusmoura13@gmail.com>
2023-12-06 21:09:00 -0300

    Aual de inicio da biblioteca pandas

--- Exercicio-07.py
+++ Exercicio-07.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 
-data = {"apples": [3,2,0,1],
-        "oranges": [0,3,7,2]
+dados = {"Nome": ["Wendel", "Paulo", "Mikaias", "Gersoney", "Fernando"],
+        "idade": [20, 20, 21, 25, 23],
+        "cidade" : ["Camaçari", "Feira de santana", "Berimbal", "Catu de Abrante", "Jauá"]
         }
-tabela = pd.DataFrame(data)
-print(tabela)
+df = pd.DataFrame(dados)
+#print(df)
+
+for dado in df.values:
+        #print(dado)
+        print(dado[0], dado[1], dado[2]) 
